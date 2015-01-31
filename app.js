43df99ab882a7071dfa3c8df6d505e5223f4e21d
Nayeem Zen <nayeem.zen@mail.utoronto.ca>
2015-01-31 04:42:18 -0500

    Add body-parser middleware

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 var express = require('express'),
-	app = express();
+    bodyParser = require('body-parser'),
+    app = express();
+
+// Use body-parser middleware for handling POST requests
+app.use(bodyParser.json())
+
+app.post('/request', function(req, res) {
+  // Handle Twilio requests here
+});
 
 app.listen(process.env.PORT || 3000);
