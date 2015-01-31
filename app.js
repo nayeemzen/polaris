1b372cab1bf0894464b34da65e7e4a2494998dd1
Nayeem Zen <nayeem.zen@mail.utoronto.ca>
2015-01-31 06:06:10 -0500

    Create JSON echo route /api/sms

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ var express = require('express'),
     port = process.env.PORT || 3000;
 
 // Use body-parser middleware for handling POST requests
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
 app.use(bodyParser.json())
+
 
 app.get('/', function(req, res) {
   res.end('<h1> This is the polaris API server');
 });
 
+// Twilio SMS Handler
 app.post('/api/sms', function(req, res) {
   // Handle Twilio requests here
-  res.json({res: 'Hello World'});
+  console.log(req.body);
+  res.json(req.body);
 });
 
 app.listen(port);
