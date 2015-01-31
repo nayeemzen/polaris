88a76f745b4565e564c7ca53751569206d5db854
Nayeem Zen <nayeem.zen@mail.utoronto.ca>
2015-01-31 05:42:08 -0500

    Add start script to package.json

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    twilio = require('twilio'),
     app = express();
 
 // Use body-parser middleware for handling POST requests
@@ -7,6 +8,7 @@ app.use(bodyParser.json())
 
 app.post('/request', function(req, res) {
   // Handle Twilio requests here
+
 });
 
 app.listen(process.env.PORT || 3000);
