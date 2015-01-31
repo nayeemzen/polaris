884dafa2146c4ddf76a73cad2d4ed12386a4d23a
Nayeem Zen <nayeem.zen@mail.utoronto.ca>
2015-01-31 05:51:09 -0500

    Add routes

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     twilio = require('twilio'),
-    app = express();
+    app = express(),
+    port = process.env.PORT || 3000;
 
 // Use body-parser middleware for handling POST requests
 app.use(bodyParser.json())
 
-app.post('/request', function(req, res) {
-  // Handle Twilio requests here
-
+app.get('/', function(req, res) {
+  res.end('<h1> This is the polaris API server');
 });
 
-app.listen(process.env.PORT || 3000);
+app.post('/api/sms', function(req, res) {
+  // Handle Twilio requests here
+  res.json({res: 'Hello World'});
+});
+
+app.listen(port);
+console.log('listening on port: ' + port);
